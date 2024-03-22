f2b585a991e5a978af05b10dd73b9743fd933dac
Ci7rix <Ci7rix@users.noreply.github.com>
2024-03-22 08:28:08 +0100

    Fixing timezone script

--- scripts/locationd.sh
+++ scripts/locationd.sh
@@ -1,6 +1,21 @@
-#! /bin/zsh
-# Enable location service
-/usr/bin/defaults write /var/db/locationd/Library/Preferences/ByHost/com.apple.locationd LocationServicesEnabled -bool true; /bin/launchctl kickstart -k system/com.apple.locationd 2>&1
-# Set auto timezone
-/usr/sbin/systemsetup -setusingnetworktime on
+#!/bin/bash
+
+uuid=$("/usr/sbin/system_profiler" SPHardwareDataType | grep "Hardware UUID" | awk '{ print $3 }')
+timedPrefs="/private/var/db/timed/Library/Preferences/com.apple.timed.plist"
+dateTimePrefs="/private/var/db/timed/Library/Preferences/com.apple.preferences.datetime.plist"
+locationPrefs="/private/var/db/locationd/Library/Preferences/ByHost/com.apple.locationd.${uuid}"
+byHostPath="/var/db/locationd/Library/Preferences/ByHost/com.apple.locationd"
+
+# Set preferences.
+echo "Ensuring location services are enabled"
+sudo -u "_locationd" /usr/bin/defaults -currentHost write "$locationPrefs" LocationServicesEnabled -int 1
+sudo defaults write "${byHostPath}" LocationServicesEnabled -int 1
+/usr/sbin/chown "_locationd:_locationd" "$locationPrefs"
+
+echo "Configuring automatic time"
+/usr/bin/defaults write "$timedPrefs" TMAutomaticTimeZoneEnabled -bool YES
+/usr/bin/defaults write "$timedPrefs" TMAutomaticTimeOnlyEnabled -bool YES
+/usr/bin/defaults write "$dateTimePrefs" timezoneset -bool YES
+/usr/sbin/chown "_timed:_timed" "$timedPrefs" "$dateTimePrefs"
+
 exit 0
